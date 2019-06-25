0d029ccad3f4c01abd01e4bd8ceb90b07b696aa1
Andrew G <booleanorama@gmail.com>
2019-06-24 22:20:37 -0400

    Weather function added

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ app.listen(port, () => {
 
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
+
+app.get('/weather/:latlon', async (request, response ) =>{
+	console.log(request.params);
+	const latlon = request.params.latlon.split(',');
+	const lat = latlon[0];
+	const lon = latlon[1];
+	//console.log(lat, lon);
+	const api_key = '70cd0eaac6d205a0c220d6780f27c514';
+	const weather_url = `https://api.darksky.net/forecast/${api_key}/${lat},${lon}`;
+	const weather_response = await fetch(weather_url);
+	const weather_data = await weather_response.json();
+
+	const data = {
+		weather: weather_data
+	};
+	response.json(data);
+});
--- public/client.js
+++ public/client.js
@@ -2,11 +2,22 @@ if('geolocation' in navigator){
     console.log('geolocation available');
 
     navigator.geolocation.getCurrentPosition(async position => {
-        let lat, lon;
+        let lat, lon, weather;
         lat = position.coords.latitude;
         lon = position.coords.longitude;
-        document.getElementById('latitude').textContent = lat;
-        document.getElementById('longitude').textContent = lon;
+        document.getElementById('latitude').textContent = lat.toFixed(3);
+        document.getElementById('longitude').textContent = lon.toFixed(3);
+
+        const api_url =`weather/${lat},${lon}`
+        const response = await fetch(api_url);
+        const json = await response.json();
+
+        weather = json.weather.currently;
+        console.log(weather);
+
+        document.getElementById('summary').textContent = weather.summary;
+        document.getElementById('temp').textContent = weather.temperature;
+
     })
 
 } else {
